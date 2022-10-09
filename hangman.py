5c909c9c1d729ff00b4af896b28a13fdbcecd367
loglas <113250057+loglas@users.noreply.github.com>
2022-10-09 13:50:34 +0100

    Add incorrect words tried
    Now incorrect words guessed are shown next to the incorrect letters guessed

--- hangman.py
+++ hangman.py
@@ -4,8 +4,12 @@ from time import sleep
 from os import name, system
 from english_words import english_words_lower_alpha_set
 
-def print_screen(man, word, letters_chosen):
+def print_screen(man, word, letters_chosen, words_chosen):
   print_letters_chosen = ""
+  print_words_chosen = ""
+  for i in range(len(words_chosen)):
+    for j in range(len(words_chosen[i])):
+      print_words_chosen += words_chosen[i][j]
   for i in range(len(letters_chosen)):
     print_letters_chosen += f"{letters_chosen[i]} "
   system(clear)
@@ -14,11 +18,14 @@ def print_screen(man, word, letters_chosen):
   for i in range(len(word)):
     whole_word = whole_word + word[i] + " "
   print(whole_word)
-  print(print_letters_chosen)
+  if len(print_letters_chosen) == 0:
+    print(print_words_chosen)
+  else:
+    print(print_letters_chosen + print_words_chosen)
 
-def guess(man, print_word, word, letters_chosen):
+def guess(man, print_word, word, letters_chosen, words_chosen):
   while True:
-    print_screen(man, print_word, letters_chosen)
+    print_screen(man, print_word, letters_chosen, words_chosen)
     guess_word = input().lower()
     if len(guess_word) == len(word):
       break
@@ -34,11 +41,15 @@ def guess(man, print_word, word, letters_chosen):
       print("You've tried that letter before!")
       sleep(2)
       continue
+    elif guess_word in words_chosen:
+      print("You've tried that word before!")
+      sleep(2)
+      continue
     else:
       break
   return guess_word
 
-def check_guess(guess_word, word, print_word, wrong, letters_chosen):
+def check_guess(guess_word, word, print_word, wrong, letters_chosen, words_chosen):
   if len(guess_word) == 1:
     if word.__contains__(guess_word):
       for i in range(len(word)):
@@ -52,13 +63,15 @@ def check_guess(guess_word, word, print_word, wrong, letters_chosen):
       for i in range(len(guess_word)):
         print_word[i] = guess_word[i]
     else:
+      words_chosen.append(guess_word)
       wrong += 1
-  return print_word, wrong, letters_chosen
+  return print_word, wrong, letters_chosen, words_chosen
 
 def main():
   word = choice(list(english_words_lower_alpha_set))
   letters_chosen = []
   print_word = []
+  words_chosen = []
   for i in range(len(word)):
     print_word.append("_")
 
@@ -67,16 +80,16 @@ def main():
   wrong = 0
   tries = 1
   
-  guess_word = guess(man[wrong], print_word, word, letters_chosen)
-  print_word, wrong, letters_chosen = check_guess(guess_word, word, print_word, wrong, letters_chosen)
+  guess_word = guess(man[wrong], print_word, word, letters_chosen, words_chosen)
+  print_word, wrong, letters_chosen, words_chosen = check_guess(guess_word, word, print_word, wrong, letters_chosen, words_chosen)
   
   while guess_word != word and print_word.__contains__("_"):
     if wrong+1 == len(man):
       break
-    guess_word = guess(man[wrong], print_word, word, letters_chosen)
+    guess_word = guess(man[wrong], print_word, word, letters_chosen, words_chosen)
     tries += 1
-    print_word, wrong, letters_chosen = check_guess(guess_word, word, print_word, wrong, letters_chosen)
-  print_screen(man[wrong], word, letters_chosen)
+    print_word, wrong, letters_chosen, words_chosen = check_guess(guess_word, word, print_word, wrong, letters_chosen, words_chosen)
+  print_screen(man[wrong], word, letters_chosen, words_chosen)
   if wrong+1 == len(man):
     print(f"Wrong! The word was {word}")
   else:
